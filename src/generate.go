bee98f8e878b68246f4e434f74cbef79dbb1542f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-29 22:05:47 -0500

    initial generator
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/generate.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/fogleman/gg"
+)
+
+func hexToBase10(val string) int64 {
+	num, err := strconv.ParseInt(val, 16, 64)
+
+	if err == nil {
+		return num
+	}
+
+	return 0
+}
+
+// Generate generates the image
+func Generate(outfile, hashedString string) error {
+	sliceStart := hexToBase10(string(hashedString[0])) // Start of slice to get colour
+	color := hashedString[sliceStart : sliceStart+6]   // The colour of the blocks
+	threshold := sliceStart                            // The amount before a block should be drawn
+	background := "fff"                                // Background color
+	blocksize := 128                                   // Size of each square
+	dimensions := []int{1024, 1024}                    // Dimensions of image
+	curX, curY := 0, 0                                 // Current x and y values
+	index := 0                                         // Index of hashed string
+
+	if threshold >= 13 {
+		threshold = 12
+	} else if threshold <= 3 {
+		threshold = 4
+	}
+
+	if threshold >= 8 {
+		background = "000"
+	}
+
+	img := gg.NewContext(dimensions[0], dimensions[1])
+	img.DrawRectangle(0, 0, float64(dimensions[0]), float64(dimensions[1]))
+	img.SetHexColor(background)
+	img.Fill()
+
+	for curY < dimensions[1] {
+		for curX < dimensions[0] {
+			fmt.Println(hexToBase10(string(hashedString[index])))
+			if threshold >= hexToBase10(string(hashedString[index])) {
+				img.DrawRectangle(float64(curX), float64(curY), float64(blocksize), float64(blocksize))
+			}
+
+			curX += blocksize
+			index++
+
+			if index >= len(hashedString) {
+				index = 0
+			}
+		}
+		curY += blocksize
+		curX = 0
+	}
+
+	img.SetHexColor(color)
+	img.Fill()
+
+	img.SavePNG(outfile)
+
+	return nil
+}
---
+++ src/genim.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"crypto/sha512"
+	"encoding/hex"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	outfile := ""
+
+	app := &cli.App{
+		Name:  "genim",
+		Usage: "Generate an image from a string",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "out",
+				Aliases:     []string{"o"},
+				Usage:       "Output generated PNG to `FILE`",
+				Destination: &outfile,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			instring := strconv.FormatInt(time.Now().UnixNano(), 10)
+
+			if c.NArg() > 0 {
+				instring = c.Args().Get(0)
+			}
+
+			hasher := sha512.New()
+			_, err := hasher.Write([]byte(instring))
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			hashedString := hex.EncodeToString(hasher.Sum(nil))
+
+			err = Generate(outfile, hashedString)
+
+			return err
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
